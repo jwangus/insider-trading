e0977a38ce8e0de66997296f585c3f2a63a51c36
Jim Wang <jim.wang.nj@gmail.com>
2022-05-18 21:39:40 -0400

    skip download error

--- download_daily_form4.py
+++ download_daily_form4.py
@@ -7,6 +7,28 @@ from datetime import date
 from secedgar import DailyFilings
 from secedgar.client import NetworkClient
 
+class SkipErrorNetworkClient(NetworkClient):
+    @staticmethod
+    async def fetch(link, session):
+        """Asynchronous get request.
+
+        Args:
+            link (str): URL to fetch.
+            session (aiohttp.ClientSession): Asynchronous client session to use to perform
+                get request.
+
+        Returns:
+            Content: Contents of response from get request.
+        """
+        try:
+            async with await session.get(link) as response:
+                contents = await response.read()
+        except:
+            logging.warn("Cannot download link: " + link)
+            contents = ""
+
+        return contents
+
 
 def download_form4(report_date):
     def _filter_for_form4(filing_entry):
@@ -14,8 +36,9 @@ def download_form4(report_date):
 
     # retry interval in seconds increases  2**n * backoff_factor
     daily_filings = DailyFilings(date=report_date,
-                                 client=NetworkClient(
+                                 client=SkipErrorNetworkClient(
                                      user_agent=USER_AGENT_EMAIL,
+                                     retry_count=4,
                                      backoff_factor=5.0,
                                      rate_limit=5),
                                  entry_filter=_filter_for_form4)
